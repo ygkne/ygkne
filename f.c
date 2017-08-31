e2c6b2a703a0c3db1ae70245d48cff2bb0efe10d
ygkne <31403283+ygkne@users.noreply.github.com>
2017-08-31 13:22:45 +0200

    Update f.c

--- f.c
+++ f.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+  
+  
+  
+  
+  int factorial (int n, int total){
+    
+    do{
+      total= n*factorial(n-1);
+    }while(n>=2);
+    
+  }
 
+
+
+int main(void){
+  
+  int factorial=0;
+  
+  
+  
+  
+  
+  
+  
+  system("PAUSE");
+  return 0;  
+}
