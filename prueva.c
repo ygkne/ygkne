e0e3c9c5a308e00d6f8c8199d421eb95a45afa4a
ygkne <31403283+ygkne@users.noreply.github.com>
2017-08-31 09:13:28 +0200

    Update prueva.c

--- prueva.c
+++ prueva.c
@@ -3,42 +3,13 @@
 #include<time.h>
 
 
-
-void desordenar (char cadena[]){
-	//definitions
-	char aux; 
-	int aleatorio = rand()%strlen(cadena);
-
-	// first change
-	aux  = cadena[aleatorio];
-	cadena[aleatorio ] =cadena[0];
-	cadena[0] = aux;
+ int maximo){
 	
-	//second change
-	aleatorio = rand()%strlen(cadena);
-	aux = cadena[aleatorio];
-	cadena[aleatorio] = cadena[1];
-	cadena[1] = aux;
-
-}
-
-
-void generar (char cadena[], int maximo){
+	cadena[0] =and()%10;
 	
-	cadena[0] = 48 + rand()%10;
-	
-	switch(rand()%3){
-		case 0:
-		 cadena[1] = rand()%15 + 33;
-		 break;
-		case 1:
-		 cadena[1] = rand()%6 + 91;
-		 break;
-		default:
-		 cadena[1] = rand()%5 + 58;
-	}
-	for(i=2;i<=maximo;i++){
-		cadena[i] = 65 + rand()%57;
+	switch(rand()%] = rand;
+	 rand()%6 + 9lt: cadena()%5 + 58	}
+	for(i=2;i<=max7;
 	}
 }
 
@@ -46,23 +17,17 @@ void generar (char cadena[], int maximo){
 
 
 int main{
-	srand(time(NULL));
-	int num=2;
-	while(num<5){
-		printf("Numero de caracteres que necesitas: ");
-		scanf(&num, %d);
+	srand(tim=2;
+	while(num{
+		printf("Numero deitas: ");
+		scad);
 	}
 
-	char password[num];
+	crd[num];
 	
-	generar(passowrd);
-	desordenar(password);
-	
-	
-	
-system("PAUSE");
-return 0;
-}
+	gen
+systSE");
+r
 ---
 	
 #include<stdio.h>
