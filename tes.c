8237ca25948a1c95eb045ac0f355206e456a1afa
ygkne <31403283+ygkne@users.noreply.github.com>
2017-08-31 10:50:49 +0200

    Update tes.c

--- tes.c
+++ tes.c
@@ -2,12 +2,16 @@
 #include<stdlib.h>
 
 int main(void){
-
-
-
-
-
-
-
-
+    
+    
+    printf("Hello, tell me about yourself. How old are you?: ");
+    
+    
+    
+    
+    
+    
+    
+    system("PAUSE");
+    return 0;
 }
